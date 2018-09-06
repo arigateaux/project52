e84f24c2fe25e21b53085175d793df6876ee2d0a
Arigateaux <dvanfoss@gmail.com>
2018-09-06 09:18:23 -0400

    Updated files.

---
+++ week-1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/arigateaux/project-52/week-1/funnel"
+)
+
+func main() {
+	fmt.Println(
+		funnel.Funnel("leave", "eave"),     // => true
+		funnel.Funnel("reset", "rest"),     // => true
+		funnel.Funnel("dragoon", "dragon"), // => true
+		funnel.Funnel("eave", "leave"),     // => false
+		funnel.Funnel("sleet", "lets"),     // => false
+		funnel.Funnel("skiff", "ski"),      // => false
+	)
+}
